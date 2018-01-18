652e55748566179a60e8e27ae26c7314ce441ddc
BaiFanJiuShuang <35549934+BaiFanJiuShuang@users.noreply.github.com>
2018-01-18 10:17:54 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,210 @@
-# kubernetes
+Table of Contents
+=================
+* [Intro](#intro)
+* [Instructions (Quick Start)](#instructions-quick-start)
+* [Other Tools](#other-tools)
+  * [Tools](#tools)
+    * [Release Notes Gathering](#release-notes-gathering)
+
+# Kubernetes Release Process
+
+This repo contains the release infrastructure for
+[Kubernetes](https://github.com/kubernetes/kubernetes).
+
+## Intro
+
+Live Kubernetes releases are done by the Kubernetes team at Google due to
+permissions and other restrictions.  This may expand eventually to allow
+other Kubernetes contributors to generate releases.
+
+The current tooling runs by default in *mock* mode and anyone should
+be able to run it in this mode to see exactly how the process works.
+In *mock* mode all the code paths are followed for a release, but nothing
+is pushed to repositories.
+
+Sticking with the ancient Greek theme, the release script is called `anago`.
+Anago means, in the context of navigators and shipping:
+"to launch out, set sail, put to sea."
+
+Tools in this repository includes a familiar [\*nix-style man
+page](https://github.com/kubernetes/release/blob/master/anago) with usage,
+process and examples.  The link shows how the self-contained doc/man page
+makes up the header of the script itself and the same info is available
+on the command-line (or get usage simply by calling the script with no options):
+
+```
+$ anago -man
+```
+
+The idea is that no external doc updates should be necessary and the
+tool itself contains all of the details and instructions and prerequisite
+checks needed for anyone to run the tool in at least mock mode.
+
+There is a simple $USER check to ensure that no one but a certain few people can
+run the script with --nomock to perform a real release.
+
+**NEW**
+`anago` is entirely re-entrant with both stateful and stateless steps.
+Upon initial execution, a WORKFLOW TOC (table of contents) is provided that
+enumerates the stateful steps that will be followed based on context and
+command-line args.  NOTE: Stateless steps are not called out in the TOC.
+* Completed steps will show with a checkmark
+* Not-yet-run steps will show a box
+* Stateful steps will show a TITLE and index (n/N) to show progress
+* /tmp/anago-runstate contains the state in the following format:
+  - CMDLINE - stores the original *critical* command-line entries 
+  - entry_points+$label - Entry points will appear as they are completed with
+an optional +label to distinguish those that are used with arguments
+  - entry_points+$label name=value - Entry points that set stateful name=value
+    pairs will have the name=value on the same line
+* Use --clean to reset the state and start over
+
+
+## Instructions (Quick Start)
+
+The tool was designed to require minimal inputs.
+The only information the tool needs is to know where you want to create a
+release with one optional flag `[--official]` \(used on release-\* branches only\).
+The [--official] flag indicates that a standard patch release will be created
+on a branch.  Without the [--official] flag, a beta would be created.
+
+There are two workflows you can choose from:
+1. Run through a complete release end to end
+2. Create staged (--stage) releases and release from there
+
+*NOTE:* Again, the tooling works by default in *mock mode* and runs in *"full
+production"* mode using `--nomock`.
+
+*IMPORTANT:* Staging and release workflows operate exclusively in either mock
+or `--nomock` modes.  If you stage something in mock mode, it is not available
+in `--nomock` mode.  Be sure to both stage and release with or without
+`--nomock`.
+
+First try a (mock) staged alpha release:
+```
+$ anago master --stage
+```
+Later, release the staged bits:
+(Artifacts are cached both locally and on GCS)
+```
+# Set --buildversion based on the final output of the staging build
+$ anago master --buildversion=<staging build version>
+```
+
+Try a beta release on a branch:
+```
+$ anago release-1.2
+```
+
+Try an official release on a branch:
+```
+$ anago release-1.2 --official
+```
+
+Try a beta release on a new branch:
+```
+$ anago release-9.9
+```
+
+NOTE: You can first stage and then release on master or any supported branch.
+
+Try creating a new branch and beta for an emergency zero-day fix.
+See [docs/branching.md](docs/branching.md) for more details.
+
+(The branch name should reflect the branch point/tag. So if branching at the
+v9.9.9 tag on the release-9.9 branch, create a release-9.9.9 branch):
+```
+$ anago release-9.9.9
+```
+
+## Typical Workflows
+
+Stage a (mock) official patch release on your local disk:
+```
+# add --build-at-head to force a build, otherwise rely on find_green_build
+# in-line to find a build candidate
+$ anago release-1.8 --stage
+```
+
+Release previously (mock) staged official patch release from your local disk:
+```
+# $buildversion will come from the output at the end of the above --stage run
+# as will this command-line in its entirety
+$ anago release-1.8 --buildversion=$buildversion
+```
+
+
+## Live Releases
+
+Anago is currently locked down to only run for a specific set of individuals.
+when ```--nomock``` is specified.
+
+Adding that flag to the command-line indicates the release will push
+tags and artifacts.  The user is still prompted before a push occurs, however.
+
+## FAQ
+
+### How can I manually specify a build to use when anago (find_green_build) can't automatically locate a green run?
+
+The output from anago (or find_green_build) displays --buildversion in its
+output while trying to locate a build.  The user is welcome to pass any
+--buildversion value to anago to create a release at a particular hash.
+
+## Other Tools
+
+All standalone scripts have embedded man pages.  Just use `-man` to view or
+your favorite editor.
+
+### Tools
+
+* [prin](https://github.com/kubernetes/release/blob/master/prin) : What tags/releases is my PR IN?
+* [find_green_build](https://github.com/kubernetes/release/blob/master/find_green_build) : Ask Jenkins for a good build to use
+* [script-template](https://github.com/kubernetes/release/blob/master/script-template) : Generate a script template in the kubernetes/release ecosystem
+* [relnotes](https://github.com/kubernetes/release/blob/master/relnotes) : Scrape github for release notes \(See below for more info\)
+* [branchff](https://github.com/kubernetes/release/blob/master/branchff) : Fast-forward branching helper
+* [changelog-update](https://github.com/kubernetes/release/blob/master/changelog-update) : Update CHANGELOG.md version entries by rescanning github for text and label changes
+* [push-build.sh](https://github.com/kubernetes/release/blob/master/push-build.sh) : Push a developer (or CI) build up to GCS
+
+### Release Notes Gathering
+
+```
+# get details on how to use the tool
+$ relnotes -man
+$ cd /kubernetes
+
+# Show release notes from the last release on a branch to HEAD
+$ relnotes
+
+# Show release notes from the last release on a specific branch to branch HEAD
+$ relnotes --branch=release-1.2
+
+# Show release notes between two specific releases
+$ relnotes v1.2.0..v1.2.1 --branch=release-1.2
+```
+
+Please report *any* [issues](https://github.com/kubernetes/release/issues)
+you encounter.
+
+## Building Packages
+
+### For Debian
+
+You can build the deb packages in a Docker container like this:
+```
+docker build --tag=debian-packager debian
+docker run --volume="$(pwd)/debian:/src" debian-packager
+```
+
+The build runs for a while, after it's done you will find the output in `debian/bin`.
+
+### For Fedora, CentOS, Red Hat Enterprise Linux
+
+You can build the rpm packages in a Docker container with:
+
+```
+cd rpm
+./docker-build.sh
+```
+
+Resulting rpms, and a pre-generated yum repository will be generated in rpm/output/x86_64.
+
---
+++ push-build.sh
@@ -0,0 +1,242 @@
+#!/bin/bash
+#
+# Copyright 2016 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Set PROGram name
+PROG=${0##*/}
+########################################################################
+#+
+#+ NAME
+#+     $PROG - Push Kubernetes Release Artifacts up to GCS
+#+
+#+ SYNOPSIS
+#+     $PROG  [--nomock] [--federation] [--noupdatelatest] [--ci]
+#+            [--bucket=<GS bucket>]
+#+            [--private-bucket]
+#+     $PROG  [--helpshort|--usage|-?]
+#+     $PROG  [--help|-man]
+#+
+#+ DESCRIPTION
+#+     Replaces kubernetes/build/push-*-build.sh.
+#+     Used for pushing developer builds and Jenkins' continuous builds.
+#+
+#+     Developer pushes simply run as they do pushing to devel/ on GCS.
+#+     In --ci mode, $PROG runs in mock mode by default.  Use --nomock to do
+#+     a real push.
+#+
+#+     Federation values are just passed through as exported global vars still
+#+     due to the fact that we're still leveraging the existing federation
+#+     interface in kubernetes proper.
+#+
+#+ OPTIONS
+#+     [--nomock]                - Enables a real push (--ci only)
+#+     [--federation]            - Enable FEDERATION push
+#+     [--ci]                    - Used when called from Jenkins (for ci runs)
+#+     [--extra-publish-file=]   - Used when need to upload additional version 
+#+                                 file to GCS. The path is relative and is 
+#+                                 append to a GCS path. (--ci only)
+#+     [--bucket=]               - Specify an alternate bucket for pushes
+#+     [--release-type=]         - Override auto-detected release type
+#+                                 (normally devel or ci)
+#+     [--release-kind=]         - Kind of release to push to GCS. Supported
+#+                                 values are kubernetes(default) or federation.
+#+     [--gcs-suffix=]           - Specify a suffix to append to the upload
+#+                                 destination on GCS.
+#+     [--docker-registry=]      - If set, push docker images to specified
+#+                                 registry/project
+#+     [--version-suffix=]       - Append suffix to version name if set.
+#+     [--noupdatelatest]        - Do not update the latest file
+#+     [--private-bucket]        - Do not mark published bits on GCS as
+#+                                 publicly readable.
+#+     [--help | -man]           - display man page for this script
+#+     [--usage | -?]            - display in-line usage
+#+
+#+ EXAMPLES
+#+     $PROG                     - Do a developer push
+#+     $PROG --nomock --federation --ci
+#+                               - Do a (non-mocked) CI push with federation
+#+     $PROG --bucket=kubernetes-release-$USER
+#+                               - Do a developer push to
+#+                                 kubernetes-release-$USER
+#+
+#+ FILES
+#+
+#+ SEE ALSO
+#+     common.sh                 - base function entry points
+#+     releaselib.sh             - ::release:: namespace entry points
+#+     kubernetes/hack/jenkins/build.sh
+#+                               - caller
+#+
+#+ BUGS/TODO
+#+     * Should federation be pulled into release repo?  It would offer more
+#+       control.
+#+
+########################################################################
+# If NO ARGUMENTS should return *usage*, uncomment the following line:
+#usage=${1:-yes}
+
+source $(dirname $(readlink -ne $BASH_SOURCE))/lib/common.sh
+source $TOOL_LIB_PATH/gitlib.sh
+source $TOOL_LIB_PATH/releaselib.sh
+
+##############################################################################
+# Initialize logs
+##############################################################################
+# Initialize and save up to 10 (rotated logs)
+MYLOG=$TMPDIR/$PROG.log
+common::logfileinit $MYLOG 10
+
+# BEGIN script
+common::timestamp begin
+
+###############################################################################
+# MAIN
+###############################################################################
+RELEASE_BUCKET=${FLAGS_bucket:-"kubernetes-release-dev"}
+# Compatibility with incoming global args
+[[ $KUBE_GCS_UPDATE_LATEST == "n" ]] && FLAGS_noupdatelatest=1
+
+# Default to kubernetes
+: ${FLAGS_release_kind:="kubernetes"}
+
+# This will canonicalize the path
+KUBE_ROOT=$(pwd -P)
+
+USE_BAZEL=false
+if release::was_built_with_bazel $KUBE_ROOT; then
+  USE_BAZEL=true
+  bazel build //:version
+  LATEST=$(cat $KUBE_ROOT/bazel-genfiles/version)
+else
+  LATEST=$(tar -xzf $KUBE_ROOT/_output/release-tars/$FLAGS_release_kind.tar.gz $FLAGS_release_kind/version -O)
+fi
+
+if [[ "$LATEST" =~ (${VER_REGEX[release]}(\.${VER_REGEX[build]})?(-dirty)?) ]]; then
+  if ((FLAGS_ci)) && [[ "$LATEST" =~ "dirty" ]]; then
+    logecho "Refusing to push dirty build with --ci flag given."
+    logecho "CI builds should always be performed from clean commits."
+    logecho
+    logecho "version output:"
+    logecho $LATEST
+    common::exit 1
+  fi
+else
+  logecho "Unable to get latest version from build tree!"
+  logecho
+  logecho "version output:"
+  logecho $LATEST
+  common::exit 1
+fi
+
+if [[ -n "${FLAGS_version_suffix:-}" ]]; then
+  LATEST+="-${FLAGS_version_suffix}"
+fi
+
+GCS_DEST="devel"
+((FLAGS_ci)) && GCS_DEST="ci"
+GCS_DEST=${FLAGS_release_type:-$GCS_DEST}
+GCS_DEST+="$FLAGS_gcs_suffix"
+GCS_EXTRA_PUBLISH_FILE=${FLAGS_extra_publish_file:-}
+
+if ((FLAGS_nomock)); then
+  logecho
+  logecho "$PROG is running a *REAL* push!!"
+  logecho
+else
+  # Point to a $USER playground
+  RELEASE_BUCKET+=-$USER
+fi
+
+##############################################################################
+common::stepheader CHECK PREREQUISITES
+##############################################################################
+if ! common::set_cloud_binaries; then
+  logecho "Releasing Kubernetes requires gsutil and gcloud. Please download,"
+  logecho "install and authorize through the Google Cloud SDK:"
+  logecho
+  logecho "https://developers.google.com/cloud/sdk/"
+  common::exit 1
+fi
+
+# Nothing should work without this.  The entire release workflow depends
+# on it whether running from the desktop or GCB
+GCP_USER=$($GCLOUD auth list --filter=status:ACTIVE \
+                             --format="value(account)" 2>/dev/null)
+[[ -n "$GCP_USER" ]] || common::exit 1 "Unable to set a valid GCP credential!"
+
+logecho -n "Check/make release bucket $RELEASE_BUCKET: "
+logrun -s release::gcs::ensure_release_bucket $RELEASE_BUCKET || common::exit 1
+
+# These operations can hit bumps and are re-entrant so retry up to 3 times
+max_attempts=3
+##############################################################################
+common::stepheader COPY RELEASE ARTIFACTS
+##############################################################################
+attempt=0
+while ((attempt<max_attempts)); do
+  if $USE_BAZEL; then
+    release::gcs::bazel_push_build $GCS_DEST $LATEST $KUBE_ROOT/_output \
+                                   $RELEASE_BUCKET && break
+  else
+    release::gcs::locally_stage_release_artifacts $GCS_DEST $LATEST \
+                                                  $KUBE_ROOT/_output \
+                                                  $FLAGS_release_kind
+    release::gcs::push_release_artifacts \
+     $KUBE_ROOT/_output/gcs-stage/$LATEST \
+     gs://$RELEASE_BUCKET/$GCS_DEST/$LATEST && break
+  fi
+  ((attempt++))
+done
+((attempt>=max_attempts)) && common::exit 1 "Exiting..."
+
+if [[ -n "${FLAGS_docker_registry:-}" ]]; then
+  ##############################################################################
+  common::stepheader PUSH DOCKER IMAGES
+  ##############################################################################
+  # TODO: support Bazel too
+  # Docker tags cannot contain '+'
+  release::docker::release $FLAGS_docker_registry ${LATEST/+/_} \
+    $KUBE_ROOT/_output
+fi
+
+# If not --ci, then we're done here.
+((FLAGS_ci)) || common::exit 0 "Exiting..."
+
+if ! ((FLAGS_noupdatelatest)); then
+  ##############################################################################
+  common::stepheader UPLOAD to $RELEASE_BUCKET
+  ##############################################################################
+  attempt=0
+  while ((attempt<max_attempts)); do
+    release::gcs::publish_version $GCS_DEST $LATEST $KUBE_ROOT/_output \
+                                  $RELEASE_BUCKET $GCS_EXTRA_PUBLISH_FILE && break
+    ((attempt++))
+  done
+  ((attempt>=max_attempts)) && common::exit 1 "Exiting..."
+fi
+
+# Leave push-federation-images.sh for now.  Not sure if this makes sense to
+# pull into the release repo.
+if ((FLAGS_federation)); then
+  ############################################################################
+  common::stepheader PUSH FEDERATION
+  ############################################################################
+  logecho -n "Push federation images: "
+  # FEDERATION_PUSH_REPO_BASE should be set by the calling job (yaml)
+  logrun -s ${KUBE_ROOT}/federation/develop/push-federation-images.sh
+fi
+
+# END script
+common::timestamp end
